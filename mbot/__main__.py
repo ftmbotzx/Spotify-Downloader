9874d2d6211537d8d078a170dff4da581affa049
ftmbotzx <kumarasta91@gmail.com>
2025-04-25 13:17:09 +0530

    Update __main__.py

--- mbot/__main__.py
+++ mbot/__main__.py
@@ -5,5 +5,18 @@ from os import path, mkdir
 if __name__ == "__main__":
     if not path.exists("cache"):
         mkdir("cache")
+
     bot = Mbot()
-    asyncio.run(bot.start())
+
+    loop = asyncio.get_event_loop()
+
+    # Create task to start the bot
+    loop.create_task(bot.start())
+
+    # Keep the loop running forever
+    try:
+        loop.run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        loop.run_until_complete(bot.stop())
+
+## from os import sys,mkdir,path
