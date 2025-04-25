f1c2b9d0eb716769944aa726e26280fa3a1eae35
ftmbotzx <kumarasta91@gmail.com>
2025-04-25 12:52:51 +0530

    Update __init__.py

--- mbot/__init__.py
+++ mbot/__init__.py
@@ -1,25 +1,3 @@
-"""MIT License
-
-Copyright (c) 2022 Daniel
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 
 from pyrogram import Client
 from os import environ,sys,mkdir,path
@@ -132,7 +110,7 @@ class Mbot(Client):
 # =============================[ UPTIME ISSUE FIXED ]================================#
 
 
-RENDER_EXTERNAL_URL = environ.get("RENDER_EXTERNAL_URL", "http://localhost:5000")
+RENDER_EXTERNAL_URL = environ.get("RENDER_EXTERNAL_URL", "https://spotify-downloader-1.onrender.com")
 
 def ping_self():
     url = f"{RENDER_EXTERNAL_URL}/alive"
